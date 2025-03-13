c2329f6481f502e24e091c10d90289883e6c5c44
DevenderDhami <devender.dhami.99@gmail.com>
2025-03-13 17:16:57 +0530

    img

---
+++ app/img-crop/page.jsx
@@ -0,0 +1,10 @@
+import ImageCropper from '@/components/ImageCropper'
+import React from 'react'
+
+const ImageCropPage = () => {
+  return (
+    <div><ImageCropper/></div>
+  )
+}
+
+export default ImageCropPage
