8baf2f1579318830d343796db55823b9330ed158
dev <dev882624@gmail.com>
2025-04-14 16:42:07 +0530

    cleer remvoed

--- utils/roles.js
+++
@@ -1,6 +0,0 @@
- import { auth } from '@clerk/nextjs/server'
- 
- export const checkRole = async (role) => {
-   const { sessionClaims } = await auth()
-   return sessionClaims?.metadata.role === role
- }
